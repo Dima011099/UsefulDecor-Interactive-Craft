dab18de62fa9768a9de05d8035e01931ef64d5a9
Dima011099 <uengein4@gmail.com>
2026-03-22 00:01:25 +0300

    Start update for 26.1-rc2 [skip ci]

--- src/main/java/com/dweb/useful_interactive/block/cabinet/BoxCabinetBlock.java
+++ src/main/java/com/dweb/useful_interactive/block/cabinet/BoxCabinetBlock.java
@@ -1,23 +1,28 @@
 package com.dweb.useful_interactive.block.cabinet;
 
-import net.minecraft.block.BlockState;
+//import net.minecraft.block.BlockState;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.level.border.WorldBorder.Settings;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
 
 import com.dweb.useful_interactive.block.chest.BoxBlock;
 
-import net.minecraft.block.Block;
+/*import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.shape.VoxelShape;
+//import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.ShapeContext;*/
 
 public class BoxCabinetBlock extends BoxBlock { 
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 0.0, 8.0, 16.0, 16.0, 16.0);
+   // private static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 0.0, 8.0, 16.0, 16.0, 16.0);
+   private static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 0.0, 8.0, 16.0, 16.0, 16.0);
 
     public BoxCabinetBlock(Settings settings) {
         super(settings);
         this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
+   
 
     private VoxelShape rotateShape(Direction direction) {
         switch (direction) {
--- src/main/java/com/dweb/useful_interactive/ui/ModScreenHandlers.java
+++ src/main/java/com/dweb/useful_interactive/ui/ModScreenHandlers.java
@@ -2,17 +2,23 @@ package com.dweb.useful_interactive.ui;
 
 import com.dweb.useful_interactive.ui.keybox.KeyBoxScreenHandler;
 
-import net.minecraft.registry.Registry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.flag.FeatureFlags;
+
+/*import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.screen.ScreenHandlerType;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.Identifier;*/
+import net.minecraft.world.inventory.MenuType;
 
 public class ModScreenHandlers {
-    public static final ScreenHandlerType<KeyBoxScreenHandler> KEY_CABINET = Registry.register(
+    public static final MenuType<KeyBoxScreenHandler> KEY_CABINET = Registry.register(
             Registries.SCREEN_HANDLER, 
             Identifier.of("useful_interactive", "key_cabinet"), 
-            new ScreenHandlerType<>(KeyBoxScreenHandler::new, FeatureFlags.VANILLA_FEATURES)
+            new MenuType<>(KeyBoxScreenHandler::new, FeatureFlags.VANILLA_FEATURES)
     );
 
     public static void registerScreenHandlers() {}
